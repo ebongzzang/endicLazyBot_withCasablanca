85c0d2dcfe99c8ee9523cf5b7062976624920272
ebongzzang <truelove9866@gmail.com>
2017-03-09 18:08:33 +0900

    add LazyListener CLass

---
+++ restServer/LazyListener.cpp
@@ -0,0 +1,3 @@
+#include <cpprest/http_listener.h>
+
+
---
+++ restServer/LazyListener.h
@@ -0,0 +1,23 @@
+#ifndef LAZY_LISTENER_H
+#define LAZY_LISTENER_H
+
+#include <cpprest\http_listener.h>
+
+using namespace web::http::experimental::listener;
+using namespace web::http;
+using namespace web;
+
+
+class LazyListener
+{
+	public:
+	LazyListener();
+	~LazyListener();
+	protected:
+
+	private:
+	
+	
+};
+
+#endif
